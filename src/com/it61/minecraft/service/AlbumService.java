dd135b9de43988952e1bf94236ec5e161f5115bc
niuqy <niuqy@tedu.cn>
2017-08-22 12:08:16 +0800

    fix some conflict of album banner

--- src/com/it61/minecraft/service/AlbumService.java
+++ src/com/it61/minecraft/service/AlbumService.java
@@ -27,16 +27,11 @@ public interface AlbumService {
 	 * 上传图片，保存记录到数据库
 	 * @param args 要保存的图片数据，依次是userid，albumid，picname
 	 */
-<<<<<<< HEAD
 	void addPictures(List<Picture> pics)throws Exception;
-
-=======
-	void addPictures(Object[] args)throws Exception;
 	/**
 	 * 获取轮播图图片.获取规则：先从第一个相册获取，如果取够了，就返回，否则去下一个相册获取，依次类推，直到取够
 	 * @param userId 用户ID
 	 * @param size	图片个数
 	 */
 	List<Picture> getBannerPics(int userId,int count);
->>>>>>> banner
 }
